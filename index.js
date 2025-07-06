3ee58ff5df1aa2ee1fa51281ae6070c206bc1785
Azaam <53663013+aZaamBie@users.noreply.github.com>
2025-07-06 14:55:30 +0200

    Some css + importing js library
    I've added some css to neaten up and make the site look better.
    Moslty centering, using flexbox for the main content, and using shadows.
    
    I'm also in the process of importing the js library, "image-conversion" but not sucessful at the moment

---
+++ index.js
@@ -0,0 +1,7 @@
+// import * as imageConversion from 'image-conversion'; // import the libary for image conversion
+const imageConversion = require("image-conversion") // import the libary for image conversion
+
+
+function convert() {
+    console.log("converting");
+}
