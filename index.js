baa2e6da8545f791309fbf9de222a2d0008fd05a
Azaam <53663013+aZaamBie@users.noreply.github.com>
2025-07-06 18:30:36 +0200

    Javascript event handling is now working
    I forgot to add a full-stop(.) when naming the class in .querySelector()
    
    Also added an svg fopr the convert button

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // import * as imageConversion from 'image-conversion'; // import the libary for image conversion
-const imageConversion = require("image-conversion") // import the libary for image conversion
+// const imageConversion = require("image-conversion") // import the libary for image conversion
 
+const convertBTN = document.querySelector(".convertBTN"); // NOTE: ALWAYS PUT A BLOODY FULL STOP when calling a class
+// convertBTN.onclick = convert()
+convertBTN.addEventListener("click", convert) // add event listener for mouse clicks
 
 function convert() {
     console.log("converting");
+    convertBTN.innerHTML = "Converting"
 }
