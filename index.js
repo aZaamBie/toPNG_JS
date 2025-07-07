47e35147bd288266770019f3a10cc120d3ecef68
Azaam <53663013+aZaamBie@users.noreply.github.com>
2025-07-07 13:46:20 +0200

    JS now working, convert function startr
    My Javascript is working.
    
    The main function "convert()" has a few conditions and just logs to the console for the time being.
    
    Will make get getter functions

--- index.js
+++ index.js
@@ -2,10 +2,42 @@
 // const imageConversion = require("image-conversion") // import the libary for image conversion
 
 const convertBTN = document.querySelector(".convertBTN"); // NOTE: ALWAYS PUT A BLOODY FULL STOP when calling a class
+const inpURL = document.getElementById("inputURL")
 // convertBTN.onclick = convert()
+inpURL.addEventListener("submit", )
 convertBTN.addEventListener("click", convert) // add event listener for mouse clicks
 
-function convert() {
-    console.log("converting");
-    convertBTN.innerHTML = "Converting"
+function hasURL(  ) {
+
 }
+
+function convert() {
+    let url = inpURL.value
+    let fileExt = url.slice(url.length-3, url.length)
+
+    
+
+    // console.log("converting the file: " + url);
+    convertBTN.innerHTML = "Converting"
+
+    if (url != ""){
+        console.log("Can convert")
+        console.log("converting the file: " + url);
+
+        console.log(fileExt + " is file extension")
+    }
+    else if( fileExt ) { // get the last 3 characters
+
+    }
+    else{
+        console.log("Empty")
+        alert("No URL given")
+    }
+
+    
+}
+
+
+// test image (THESE ARE CC-0):
+/* https://cdn.stocksnap.io/img-thumbs/280h/house-cat_MIZQ6V1ZJU.jpg - cat
+*/
