a85a31553d8c6c620bc3cc98839923499eb0e466
Azaam <53663013+aZaamBie@users.noreply.github.com>
2025-07-07 16:04:14 +0200

    added pre-conversion checks
    I added two functions to first check whether a given URL was not empty, and another function to check whether it's not already a png file.
    
    1. Check if url length >0 to be true
    2. pass the file extension into method, and check: if method == "png" --> true
    
    Still want to add checks for whether url is not empty but isn't valid.
    And mayeb add like a small window or box that slides to the right of the url box.,
    
    And must add actual conversion

--- index.js
+++ index.js
@@ -3,35 +3,50 @@
 
 const convertBTN = document.querySelector(".convertBTN"); // NOTE: ALWAYS PUT A BLOODY FULL STOP when calling a class
 const inpURL = document.getElementById("inputURL")
-// convertBTN.onclick = convert()
-inpURL.addEventListener("submit", )
+// inpURL.addEventListener("submit", )
 convertBTN.addEventListener("click", convert) // add event listener for mouse clicks
 
-function hasURL(  ) {
+var url = inpURL.value // global / hoisted var
 
+function updateURL() {
+    url = inpURL.value
 }
 
+function hasURL(  ) { // check if the input box contains a valid url
+    if (url.length > 0){return true;}
+    return false; 
+}
+
+function isPNG(type) {
+    console.log("checking if png")
+    if (type=="png") { return true;}
+    return false;
+}
+
+
+
 function convert() {
-    let url = inpURL.value
-    let fileExt = url.slice(url.length-3, url.length)
+    updateURL() // get latest url
+    let fileExt = url.slice(url.length-3, url.length).toLowerCase()
 
     
-
     // console.log("converting the file: " + url);
-    convertBTN.innerHTML = "Converting"
-
-    if (url != ""){
+    
+    
+    if (!hasURL()){ // first check if has a URL
+       console.log("Empty / No URL")
+       alert("No URL given") 
+    } 
+    else if( isPNG(fileExt) ) { // then check if already png
+        console.log("This is already a PNG")
+        // setTimeout( close, 1000) // set timer for 1000ms(1s) and then close
+    }
+    else{ // then proceed with conversion
         console.log("Can convert")
         console.log("converting the file: " + url);
+        convertBTN.innerHTML = "Converting"
 
-        console.log(fileExt + " is file extension")
-    }
-    else if( fileExt ) { // get the last 3 characters
-
-    }
-    else{
-        console.log("Empty")
-        alert("No URL given")
+        console.log(fileExt + " is file extension") 
     }
 
     
@@ -39,5 +54,9 @@ function convert() {
 
 
 // test image (THESE ARE CC-0):
-/* https://cdn.stocksnap.io/img-thumbs/280h/house-cat_MIZQ6V1ZJU.jpg - cat
+/*  https://cdn.stocksnap.io/img-thumbs/280h/house-cat_MIZQ6V1ZJU.jpg - cat
+    https://w0.peakpx.com/wallpaper/333/363/HD-wallpaper-joker-cool.jpg - joker why so serious
 */
+
+
+// https://www.w3schools.com/js/js_timing.asp look at JS timing
