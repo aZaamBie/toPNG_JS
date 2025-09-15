5d4057d6e1567649554f3cfaa6f021f26f8fabb5
Azaam <53663013+aZaamBie@users.noreply.github.com>
2025-09-15 10:36:43 +0200

    Added getFiletype feature
    Replaced the "PNG" text in the header with a drop-down menu, in the html.
    
    JS will lsiten for changes to this menu, and set the targetType to the update, selected option

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ const urlBox = document.getElementById("urlBox")
 convertBTN.addEventListener("click", convert) // add event listener for mouse clicks
 inpURL.addEventListener("change", getPreviewName )
 
+const fTypeMenu = document.getElementById('fType-select')
+fTypeMenu.addEventListener("change", setFileType) // change better than run for this
+
 // urlBox.addEventListener("mouseover", () => previewHover(true)) // need to wrap the calls in arrow functions for it work
 // urlBox.addEventListener("mouseout", () => previewHover(false)) // ^ same here
 
 var url = inpURL.value // global / hoisted var
 var urlList = [];
 
-var targetType = "png"
+var targetType = "png" // default
 
 // check if Library is loaded
 
@@ -53,6 +56,7 @@ function updateURL() {
     // console.log(urlList)
 }
 
+// validations
 function hasURL(  ) { // check if the input box contains a valid url
     if (url.length > 0){return true;}
     return false; 
@@ -62,7 +66,7 @@ function isPNG(type) {
     if (type=="png") { return true;}
     return false;
 }
-
+//
 
 
 async function convert() {
@@ -95,9 +99,7 @@ async function convert() {
         let filename = parts[parts.length-1]// get the substring from index 0 , until and excluding the last 4 characters (which are the full stop and file extension)
         
         let newFname= getPreviewName(filename, targetType)
-        // alert(filename) //  testing purposes
-
-        console.log(newFname + " is new name | convert()")
+        // console.log(newFname + " is new name | convert()")
         imageConversion.downloadFile(pngIMG, newFname) // download image with new filename as associated file name
         // imageConversion.downloadFile(pngIMG, "converted.png") // download with default name [ THIS WORKS]
         /* END OF 2 */
@@ -122,12 +124,18 @@ function getPreviewName(name="", type=targetType){
     let parts = name.split("/") // split the url into different segments, seperated by the forward slash
     let filename = parts[parts.length-1] // get the very last segment, which USUALLY is the filename
     let newName = filename.substring(0, filename.length-3) + type;
-    console.log(newName+ " is name | getPreviewName")
+    // console.log(newName+ " is name | getPreviewName")
 
     const fName = document.getElementById("namePreview") // get the namePreview <p> element
     fName.innerHTML = "Preview filename: " + newName
     // return fName // why was i returning the DOM element?? // 
     return newName;// BLOODY IDIOT. YOU'RE MEANT TO RETURN THE STRING, NOT THE DOM ELEMENT!!
+}
+
+function setFileType(){ //
+    let type = document.getElementById('fType-select').value.toLowerCase()
+    // console.log("Type is: " + type)
+    targetType = type
 }
 
 function previewHover(active) {
